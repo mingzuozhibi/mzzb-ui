72e3f670b824af079e86c778836ac4efb989721d
fuhaiwei <fuhaiwei@126.com>
2017-02-01 22:04:23 +0800

    feat: add paper to config page

--- src/app/pages/Configuration.js
+++ src/app/pages/Configuration.js
@@ -1,27 +1,35 @@
 import React, {PropTypes} from "react";
+import Paper from "material-ui/Paper";
 import Toggle from "material-ui/Toggle";
 
 export default class Configuration extends React.Component {
 
   static contextTypes = {
     isLight: PropTypes.bool.isRequired,
+    muiTheme: PropTypes.object.isRequired,
     handleChangeTheme: PropTypes.func.isRequired,
   };
 
   render() {
     const {isLight, handleChangeTheme} = this.context;
     const styles = {
+      block: {
+        padding: '20px 18px 4px 18px',
+        maxWidth: 250,
+      },
       toggle: {
         marginBottom: 16,
       },
     };
     return (
-      <Toggle
-        label="Simple"
-        style={styles.toggle}
-        defaultToggled={isLight}
-        onToggle={(e,v)=>handleChangeTheme(v)}
-      />
+      <Paper style={styles.block}>
+        <Toggle
+          label={isLight ? 'Light Theme' : 'Dark Theme'}
+          style={styles.toggle}
+          defaultToggled={isLight}
+          onToggle={(e, v)=>handleChangeTheme(v)}
+        />
+      </Paper>
     )
   }
 
