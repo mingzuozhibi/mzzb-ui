8706e8e03c04ddc313ee7129a9b8e2bf451b7f08
fuhaiwei <fuhaiwei@126.com>
2017-02-03 09:30:30 +0800

    refactor: use const replace var

--- src/app/utils/Ajax.js
+++ src/app/utils/Ajax.js
@@ -8,7 +8,7 @@ function checkStatus(response) {
   if (response.status >= 200 && response.status < 300) {
     return response;
   } else {
-    var error = new Error(response.statusText);
+    const error = new Error(response.statusText);
     error.response = response;
     throw error;
   }
