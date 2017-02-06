0252b0b2973c263b8b4351156155cc5b242d2ed7
fuhaiwei <fuhaiwei@126.com>
2017-02-06 16:54:56 +0800

    refactor: rename styles to layout

--- src/app/Master.jsx
+++ src/app/Master.jsx
@@ -5,10 +5,10 @@ import getMuiTheme from "material-ui/styles/getMuiTheme";
 import spacing from "material-ui/styles/spacing";
 import darkTheme from "material-ui/styles/baseThemes/darkBaseTheme";
 import withWidth, {MEDIUM, LARGE} from "material-ui/utils/withWidth";
-import AppActionBar from "./styles/AppActionBar.jsx";
-import AppNavDrawer from "./styles/AppNavDrawer.jsx";
-import AppLoginDialog from "./styles/AppLoginDialog.jsx";
-import AppAlertDialog from "./styles/AppAlertDialog.jsx";
+import AppActionBar from "./layout/AppActionBar.jsx";
+import AppNavDrawer from "./layout/AppNavDrawer.jsx";
+import AppLoginDialog from "./layout/AppLoginDialog.jsx";
+import AppAlertDialog from "./layout/AppAlertDialog.jsx";
 import {activePage} from "./utils/Page";
 import {sessionManager} from "./Api";
 
