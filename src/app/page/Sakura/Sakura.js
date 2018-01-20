e0281a6e23470928ee206aeeafa92bef1d079b00
fuhaiwei <fuhaiwei@126.com>
2018-01-20 19:17:34 +0800

    feat: add bootstrap table

--- src/app/page/Sakura/Sakura.js
+++ src/app/page/Sakura/Sakura.js
@@ -1,37 +1,75 @@
 import React from 'react'
 import Button from 'material-ui/FloatingActionButton'
 import UpdateIcon from 'material-ui/svg-icons/action/update'
+import './bootstrap.min.css'
 
-function Sakura({doFetchSakuraData, sakuraLists}) {
-  sakuraLists.map(list => list.discs).forEach(discs => {
-    discs.sort((a, b) => a.this_rank - b.this_rank)
+class Column {
+  constructor({className, compare, format, style = {}, title}) {
+    this.className = className
+    this.compare = compare
+    this.format = format
+    this.style = style
+    this.title = title
+  }
+}
+
+const columns = [
+  new Column({
+    className: 'rank',
+    title: '日亚排名',
+    style: {width: '165px'},
+    format: disc => `${disc['this_rank']}位/${disc['prev_rank']}位`,
+  }),
+  new Column({
+    className: 'sumpt',
+    title: '累积PT',
+    style: {width: '105px'},
+    format: disc => `${disc['total_point']} pt`,
+  }),
+  new Column({
+    className: 'sday',
+    title: '剩余天数',
+    style: {width: '95px'},
+    format: disc => `${disc['surplus_days']}天`,
+  }),
+  new Column({
+    className: 'title',
+    title: '碟片标题',
+    format: disc => disc['title'],
+  }),
+]
+
+function Sakura({doFetchSakuraData, data}) {
+  data.map(list => list['discs']).forEach(discs => {
+    discs.sort((a, b) => a['this_rank'] - b['this_rank'])
   })
-  const style = {
+  const updateStyle = {
     position: 'fixed',
     bottom: '30px',
     right: '10px'
   }
+  const tableClass = 'table table-striped table-bordered'
   return (
     <div id="sakura">
-      <Button style={style} onTouchTap={doFetchSakuraData}>
+      <Button style={updateStyle} onTouchTap={doFetchSakuraData}>
         <UpdateIcon/>
       </Button>
-      {sakuraLists.map(sakuraList => (
-        <table key={sakuraList.name} border="1">
-          <caption>{sakuraList.title}</caption>
+      {data.map(list => (
+        <table className={tableClass} key={list['name']}>
+          <caption>{list['title']}</caption>
           <thead>
           <tr>
-            <th>rank</th>
-            <th>prev</th>
-            <th>title</th>
+            {columns.map(c => (
+              <th className={c.className} style={c.style}>{c.title}</th>
+            ))}
           </tr>
           </thead>
           <tbody>
-          {sakuraList.discs.map(disc => (
-            <tr key={disc.asin}>
-              <td>{disc.this_rank}</td>
-              <td>{disc.prev_rank}</td>
-              <td>{disc.title}</td>
+          {list['discs'].map(disc => (
+            <tr key={disc['asin']}>
+              {columns.map(c =>
+                (<td className={c.className}>{c.format(disc)}</td>)
+              )}
             </tr>
           ))}
           </tbody>
