8c838535d97be4592f1a533ce1de30bf6a6c2e6b
fuhaiwei <fuhaiwei@126.com>
2018-02-04 23:00:56 +0800

    feat: add async component

--- src/app/route.js
+++ src/app/route.js
@@ -1,15 +1,42 @@
-import React from 'react'
-import {Route, IndexRoute} from 'react-router'
+import React, {Component} from 'react'
+import {IndexRoute, Route} from 'react-router'
 import App from './App'
 import Home from './page/Home'
-import Sakura from './page/Sakura'
 
 const route = (
   <Route path="/" component={App}>
     <IndexRoute component={Home}/>
     <Route path="home" component={Home}/>
-    <Route path="sakura" component={Sakura}/>
+    <Route path="sakura" component={asyncComponent(() => import('./page/Sakura'))}/>
   </Route>
 )
 
 export default route
+
+function asyncComponent(importComponent) {
+  class AsyncComponent extends Component {
+    constructor(props) {
+      super(props)
+
+      this.state = {
+        component: null
+      }
+    }
+
+    async componentDidMount() {
+      const {default: component} = await importComponent()
+
+      this.setState({
+        component: component
+      })
+    }
+
+    render() {
+      const C = this.state.component
+
+      return C ? <C {...this.props} /> : <h3>Loading...</h3>
+    }
+  }
+
+  return AsyncComponent
+}
