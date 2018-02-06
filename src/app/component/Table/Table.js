9a99498ceeaaa5726f5b3d86efde175b23b6d266
fuhaiwei <fuhaiwei@126.com>
2018-02-06 10:40:43 +0800

    fix: fixup table title site

--- src/app/component/Table/Table.js
+++ src/app/component/Table/Table.js
@@ -1,27 +1,29 @@
 import React from 'react'
 
 function Table({title, rows, columns}) {
-  const spanStyle = {fontSize: '24px', marginRight: '10px'}
+  const titleStyle = {fontSize: '24px', marginRight: '10px', marginBottom: '5px'}
   return (
-    <table className={'table table-striped table-bordered'}>
-      <caption><span style={spanStyle}>{title}</span></caption>
-      <thead>
-      <tr>
-        {columns.map(c => (
-          <th key={c.className} className={c.className} style={c.style}>{c.title}</th>
-        ))}
-      </tr>
-      </thead>
-      <tbody>
-      {rows.map(data =>
-        <tr key={data['id']}>
-          {columns.map(c =>
-            <td key={c.className} className={c.className}>{c.format(data)}</td>
-          )}
+    <div>
+      <div style={titleStyle}>{title}</div>
+      <table className={'table table-striped table-bordered'}>
+        <thead>
+        <tr>
+          {columns.map(c => (
+            <th key={c.className} className={c.className} style={c.style}>{c.title}</th>
+          ))}
         </tr>
-      )}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+        {rows.map(data =>
+          <tr key={data['id']}>
+            {columns.map(c =>
+              <td key={c.className} className={c.className}>{c.format(data)}</td>
+            )}
+          </tr>
+        )}
+        </tbody>
+      </table>
+    </div>
   )
 }
 
