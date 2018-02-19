f43fbdd07b536287f18b1d7b8248321569cef783
fuhaiwei <fuhaiwei@126.com>
2018-02-19 12:42:11 +0800

    feat: Manual Merge react-javascript

---
+++ bin/set_version.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# 项目根目录
+basepath=$(cd `dirname $0`; pwd)/..
+
+# 准备发布新版本
+cd ${basepath}
+git flow release start "v$1"
+
+# 更新 package.json 版本号
+npm version $1
+git commit --amend -m "chore: set version to v$1"
+git tag -d "v$1"
+
+# 发布新版本
+git flow release finish "v$1"
