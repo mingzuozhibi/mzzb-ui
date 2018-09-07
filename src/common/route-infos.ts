50052b06981d7958fc789bf4fa11dd2fab715523
fuhaiwei <fuhaiwei@126.com>
2018-09-08 01:52:42 +0800

    feat: Remove Some Zhuanlou

--- src/common/route-infos.ts
+++ src/common/route-infos.ts
@@ -69,10 +69,6 @@ export const menuInfos: MenuInfo[] = [
         path: 'https://tieba.baidu.com/p/5627916964?pn=9999',
       },
       {
-        text: '2018春-B站数据统计',
-        path: 'https://tieba.baidu.com/p/5615545349?pn=9999',
-      },
-      {
         text: '2018春-长期追踪贴',
         path: 'https://tieba.baidu.com/p/5627461603?pn=9999',
       },
@@ -80,45 +76,13 @@ export const menuInfos: MenuInfo[] = [
   },
   {
     icon: 'icon-social-tieba',
-    text: '2018春专楼',
-    path: '/zl-201804',
-    subMenus: [
-      {
-        text: '命运石之门0',
-        path: 'https://tieba.baidu.com/p/5627694697?pn=9999',
-      },
-      {
-        text: '女神异闻录5',
-        path: 'https://tieba.baidu.com/p/5627689510?pn=9999',
-      },
-      {
-        text: '刀剑神域外传',
-        path: 'https://tieba.baidu.com/p/5617733420?pn=9999',
-      },
-      {
-        text: '银河英雄传说',
-        path: 'https://tieba.baidu.com/p/5637222082?pn=9999',
-      },
-    ]
-  },
-  {
-    icon: 'icon-social-tieba',
-    text: '往季专楼',
-    path: '/zl-wangji',
+    text: '推荐专楼',
+    path: '/zhuanlou',
     subMenus: [
       /** 1801 */
       {
-        text: 'DitF(国家队)',
-        path: 'https://tieba.baidu.com/p/5500270781?pn=9999',
-      },
-      {
-        text: '紫罗兰永恒花园1.0',
+        text: '紫罗兰永恒花园',
         path: 'https://tieba.baidu.com/p/5500266707?pn=9999',
-      },
-      /** 1710 */
-      {
-        text: '少女终末旅行',
-        path: 'https://tieba.baidu.com/p/5342754125?pn=9999',
       },
       /** 1707 */
       {
@@ -130,29 +94,10 @@ export const menuInfos: MenuInfo[] = [
         text: '末日时在做什么？有没有空？可以来拯救吗？',
         path: 'https://tieba.baidu.com/p/5040597877?pn=9999',
       },
-      /** 1701 */
-      {
-        text: '兽娘动物园',
-        path: 'https://tieba.baidu.com/p/4977733257?pn=9999',
-      },
-      /** 1610 */
-      {
-        text: 'YURI!!! ON ICE 2.0',
-        path: 'https://tieba.baidu.com/p/4956531567?pn=9999',
-      },
-      /** 1607 */
-      {
-        text: '灵能百分百',
-        path: 'https://tieba.baidu.com/p/4633245390?pn=9999',
-      },
       /** 1604 */
       {
         text: 'Re:从零开始的异世界生活',
         path: 'https://tieba.baidu.com/p/4773136352?pn=9999',
-      },
-      {
-        text: '甲铁城的卡巴内利',
-        path: 'https://tieba.baidu.com/p/4652025583?pn=9999',
       },
     ]
   },
