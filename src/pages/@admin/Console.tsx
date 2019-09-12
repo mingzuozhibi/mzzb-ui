0cb86ca7c4ea0be4710af4c2af284ed14d2e1c5f
fuhaiwei <fuhaiwei@126.com>
2019-09-13 06:24:11 +0800

    feat: Update Style And Fix Bug

--- src/pages/@admin/Console.tsx
+++ src/pages/@admin/Console.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import { RouteComponentProps } from 'react-router'
 import { useData } from '../../hooks/useData'
 import { Column, Table } from '../../comps/@table/Table'
-import { RouteComponentProps } from 'react-router'
 import { CustomPagination } from '../../comps/CustomPagination'
+import './Console.scss'
+import { formatNumber } from '../../funcs/format'
 
 interface Data {
   id: number
@@ -12,7 +14,7 @@ interface Data {
   acceptOn: number,
 }
 
-export default function Console({location, history, match}: RouteComponentProps<{name: string}>) {
+export default function Console({location, history, match}: RouteComponentProps<{ name: string }>) {
 
   const [{data, page}, handler] = useData<Data[]>(`/gateway/moduleMessages/${match.params.name}${location.search}`)
 
@@ -24,11 +26,11 @@ export default function Console({location, history, match}: RouteComponentProps<
     {
       key: 'time',
       title: '时间',
-      format: t => new Date(t.createOn).toLocaleString()
+      format: formatDate
     },
     {
       key: 'text',
-      title: '消息',
+      title: '消息内容',
       format: t => t.text
     },
   ]
@@ -37,7 +39,7 @@ export default function Console({location, history, match}: RouteComponentProps<
     if (pageSize === 20) {
       history.push(`/console?page=${page}`)
     } else {
-      history.push(`/console?page=${page}&pageSize=${pageSize}`)
+      history.push(`/console/${match.params.name}?page=${page}&pageSize=${pageSize}`)
     }
   }
 
@@ -57,4 +59,14 @@ export default function Console({location, history, match}: RouteComponentProps<
       )}
     </div>
   )
+}
+
+function formatDate(t: Data) {
+  const date = new Date(t.createOn)
+  const months = formatNumber(date.getMonth(), '00')
+  const dates = formatNumber(date.getDate(), '00')
+  const hours = formatNumber(date.getHours(), '00')
+  const minutes = formatNumber(date.getMinutes(), '00')
+  const seconds = formatNumber(date.getSeconds(), '00')
+  return `${months}/${dates} ${hours}:${minutes}:${seconds}`
 }
