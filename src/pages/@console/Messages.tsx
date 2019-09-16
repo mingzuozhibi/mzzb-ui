66b43f4bf28ebee0a52b4b17c4638c444bd94a87
fuhaiwei <fuhaiwei@126.com>
2019-09-16 09:29:16 +0800

    feat: Extract Messages Page

--- src/pages/@console/Messages.tsx
+++ src/pages/@console/Messages.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import { withRouter } from 'react-router-dom'
 import { Alert } from 'antd'
 import { useData } from '../../hooks/useData'
+import { useTitle } from '../../hooks/hooks'
 import { formatNumber } from '../../funcs/format'
 import { Column, Table } from '../../comps/@table/Table'
 import { CustomPagination } from '../../comps/CustomPagination'
 import { RouteProps } from '../@types'
-import './Console.scss'
+import './Messages.scss'
 
 interface Data {
   id: number
@@ -15,36 +17,26 @@ interface Data {
   acceptOn: number,
 }
 
-function trClass(t: Data) {
-  return {
-    'warning': t.type === 'warning',
-    'success': t.type === 'success',
-    'danger': t.type === 'danger',
-    'info': t.type === 'notify',
-  }
+interface Props {
+  moduleName: string
+  tableTitle: string
 }
 
-export default function Console({location, history, match}: RouteProps<{ name: string }>) {
+export default withRouter(Messages)
 
-  const url = `/gateway/messages/${match.params.name}${location.search}`
+function Messages(props: Props & RouteProps<{ name: string }>) {
+
+  const {moduleName, tableTitle, location, history, match} = props
+  const url = `/gateway/messages/${moduleName}${location.search}`
   const [{error, data, page}, handler] = useData<Data[]>(url)
 
   data && data.forEach((d, i) => {
     d.id = i
   })
 
-  const cols: Column<Data>[] = [
-    {
-      key: 'time',
-      title: '时间',
-      format: formatDate
-    },
-    {
-      key: 'text',
-      title: '消息内容',
-      format: t => t.text
-    },
-  ]
+  const cols = getCols()
+
+  useTitle(tableTitle)
 
   function onPaginationChange(page: number, pageSize?: number) {
     if (pageSize === 20) {
@@ -55,13 +47,13 @@ export default function Console({location, history, match}: RouteProps<{ name: s
   }
 
   return (
-    <div className="Console">
+    <div className="Messages">
       {error && (
         <Alert message={error} type="error"/>
       )}
       {data && (
         <Table
-          title="系统日志"
+          title={tableTitle}
           cols={cols}
           rows={data}
           handler={handler}
@@ -75,6 +67,21 @@ export default function Console({location, history, match}: RouteProps<{ name: s
   )
 }
 
+function getCols(): Column<Data>[] {
+  return [
+    {
+      key: 'time',
+      title: '时间',
+      format: formatDate
+    },
+    {
+      key: 'text',
+      title: '消息内容',
+      format: t => t.text
+    },
+  ]
+}
+
 function formatDate(t: Data) {
   const date = new Date(t.createOn)
   const months = formatNumber(date.getMonth(), '00')
@@ -83,4 +90,13 @@ function formatDate(t: Data) {
   const minutes = formatNumber(date.getMinutes(), '00')
   const seconds = formatNumber(date.getSeconds(), '00')
   return `${months}/${dates} ${hours}:${minutes}:${seconds}`
+}
+
+function trClass(t: Data) {
+  return {
+    'warning': t.type === 'warning',
+    'success': t.type === 'success',
+    'danger': t.type === 'danger',
+    'info': t.type === 'notify',
+  }
 }
