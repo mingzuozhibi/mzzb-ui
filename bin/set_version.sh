6ec89690a0916a22849d34269d52924d3bb5edf2
fuhaiwei <fuhaiwei@126.com>
2020-05-07 16:09:49 +0800

    feat(bin): Update set_version.sh

--- bin/set_version.sh
+++ bin/set_version.sh
@@ -1,15 +1,40 @@
 #!/usr/bin/env bash
 
-# 项目根目录
 cd `dirname $0`/..
 
-# 准备发布新版本
-git flow release start "v$1"
+tagtxt="v$1"
+version="$1"
+option2="$2"
 
-# 更新 package.json 版本号
-npm version $1
-git commit --amend -m "chore: set version to v$1"
-git tag -d "v$1"
+CallRelease() {
+    git flow release start $tagtxt
 
-# 发布新版本
-git flow release finish "v$1"
+    npm version $version
+    git commit --amend -m "chore: set version to $version"
+    git tag -d $tagtxt
+
+    git flow release finish $tagtxt
+}
+
+CallPushAll() {
+  echo "开始推送分支 develop"
+  git push origin develop
+
+  echo "开始推送分支 master"
+  git push origin master
+
+  echo "开始推送标签 $tagtxt"
+  git push origin $tagtxt
+}
+
+if [[ $option2 == "-a" ]]; then
+  CallRelease
+  CallPushAll
+  exit 0
+fi
+
+if [[ $option2 == "-f" ]]; then
+  CallPushAll
+else
+  CallRelease
+fi
