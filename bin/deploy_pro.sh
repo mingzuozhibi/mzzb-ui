c19c7a1aa905fd3dc70ea975c12614da996693da
fuhaiwei <fuhaiwei@126.com>
2022-04-22 14:20:22 +0800

    chore: update shell files

--- bin/deploy_pro.sh
+++ bin/deploy_pro.sh
@@ -2,12 +2,15 @@
 
 set -e
 
-cd $(cd `dirname $0`; pwd)/..
+# 环境准备
+DirName=$(dirname "$0")
+HomeDir=$(realpath "$DirName"/..)
+cd "$HomeDir" || exit
 
+# 构建部署
 if [[ $1 != '-q' ]]; then
-  yarn build
+    yarn build
 fi
 
-ssh q "rm -rf nginx/www/mzzb-ui-pro; mkdir -p nginx/www/mzzb-ui-pro;"
-
+ssh q "rm -rf nginx/www/mzzb-ui-dev; mkdir -p nginx/www/mzzb-ui-pro;"
 tar -czf - -C build . | ssh q "tar -xzpvf - -C nginx/www/mzzb-ui-pro"
