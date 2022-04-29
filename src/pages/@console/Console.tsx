a611fb9060ee8cb31c3858f0f9d092ba592c5d54
fuhaiwei <fuhaiwei@126.com>
2022-04-29 18:37:18 +0800

    feat: update for /api/messages

--- src/pages/@console/Console.tsx
+++ src/pages/@console/Console.tsx
@@ -1,32 +1,58 @@
 import { useTitle } from '##/hooks'
 import { Tabs } from 'antd'
+import { useLocation } from 'react-router-dom'
 import Messages from './Messages'
 
 const modules = [
   {
-    moduleName: 'mzzb-disc-spider',
-    tableTitle: '排名抓取',
+    value: 'SPIDER_CONTENT',
+    label: '排名抓取',
   },
   {
-    moduleName: 'mzzb-disc-shelfs',
-    tableTitle: '上架抓取',
+    value: 'SPIDER_HISTORY',
+    label: '上架抓取',
   },
   {
-    moduleName: 'mzzb-server',
-    tableTitle: '核心模块',
+    value: 'SERVER_DISC',
+    label: '碟片日志',
+  },
+  {
+    value: 'SERVER_USER',
+    label: '用户日志',
+  },
+  {
+    value: 'SERVER_CORE',
+    label: '核心日志',
+  },
+  {
+    value: 'DEFAULT',
+    label: '其他日志',
   },
 ]
 
 export default function Console() {
   useTitle('系统日志')
 
+  function onChange(activeKey: string) {
+    window.location.hash = activeKey
+  }
+
   return (
-    <Tabs type="card" defaultActiveKey="1" onChange={undefined}>
-      {modules.map(({ moduleName, tableTitle }) => (
-        <Tabs.TabPane key={moduleName} tab={tableTitle}>
-          <Messages moduleName={moduleName} />
+    <Tabs type="card" activeKey={getDefaultValue()} onChange={onChange}>
+      {modules.map(({ value, label }) => (
+        <Tabs.TabPane key={value} tab={label}>
+          <Messages name={value} />
         </Tabs.TabPane>
       ))}
     </Tabs>
   )
+}
+
+function getDefaultValue() {
+  const value = window.location.hash
+  if (value.length > 0) {
+    return value.substring(1)
+  } else {
+    return modules[0].value
+  }
 }
--- src/pages/@console/Messages.tsx
+++ src/pages/@console/Messages.tsx
@@ -3,39 +3,47 @@ import { Column, Table } from '#C/@table/Table'
 import { CustomDate } from '#C/CustomDate'
 import { CustomLink } from '#C/CustomLink'
 import { CustomPagination } from '#C/CustomPagination'
-import { Alert, Button, Radio } from 'antd'
+import { Alert, Button, Checkbox } from 'antd'
 import { useState } from 'react'
 import './Messages.scss'
 
 interface Data {
   id: number
-  type: 'info' | 'success' | 'notify' | 'warning' | 'danger'
+  name: string
+  type: 'DEBUG' | 'INFO' | 'NOTIFY' | 'SUCCESS' | 'WARNING' | 'ERROR'
   text: string
   createOn: number
   acceptOn: number
 }
 
 interface Props {
-  moduleName: string
+  name: string
 }
 
-export default function Messages({ moduleName }: Props) {
-  const [messageType, setMessageType] = useState('info')
-  const [{ pageNumber, pageSize }, setPage] = useState({ pageNumber: 1, pageSize: 40 })
-  const url = `/api/messages/${moduleName}?type=${messageType}&page=${pageNumber}&pageSize=${pageSize}`
-  const [{ error, data, page }, handler] = useData<Data[]>(url)
+const options: any[] = [
+  { label: '调试', value: 'DEBUG' },
+  { label: '信息', value: 'INFO' },
+  { label: '通知', value: 'NOTIFY' },
+  { label: '成功', value: 'SUCCESS' },
+  { label: '警告', value: 'WARNING' },
+  { label: '错误', value: 'ERROR' },
+]
 
-  data && data.forEach((e, i) => (e.id = i))
+export default function Messages({ name }: Props) {
+  const [types, setTypes] = useState('DEBUG,INFO,NOTIFY,SUCCESS,WARNING,ERROR')
+  const [{ pageNumber, pageSize }, setPage] = useState({ pageNumber: 0, pageSize: 20 })
+  const url = `/api/messages/${name}?types=${types}&page=${pageNumber}&pageSize=${pageSize}`
+  const [{ error, data, page }, handler] = useData<Data[]>(url)
 
   const cols = getCols()
 
   function onPaginationChange(page: number, pageSize?: number) {
-    setPage({ pageNumber: page, pageSize: pageSize || 40 })
+    setPage({ pageNumber: page - 1, pageSize: pageSize || 20 })
     window.scroll(0, 0)
   }
 
-  function onChangeType(e: any) {
-    setMessageType(e.target.value)
+  function onChangeType(checked: any[]) {
+    setTypes(checked.join(','))
   }
 
   return (
@@ -49,13 +57,7 @@ export default function Messages({ moduleName }: Props) {
             loading={handler.loading}
             style={{ marginRight: 10 }}
           />
-          <Radio.Group onChange={onChangeType} value={messageType}>
-            <Radio.Button value="info">所有</Radio.Button>
-            <Radio.Button value="notify">通知</Radio.Button>
-            <Radio.Button value="success">成功</Radio.Button>
-            <Radio.Button value="warning">警告</Radio.Button>
-            <Radio.Button value="danger">错误</Radio.Button>
-          </Radio.Group>
+          <Checkbox.Group onChange={onChangeType} options={options} value={types.split(',')} />
         </div>
       )}
       {page && (
@@ -86,10 +88,11 @@ function getCols(): Column<Data>[] {
 
 function trClass(t: Data) {
   return {
-    warning: t.type === 'warning',
-    success: t.type === 'success',
-    danger: t.type === 'danger',
-    info: t.type === 'notify',
+    debug: t.type === 'DEBUG',
+    info: t.type === 'NOTIFY',
+    success: t.type === 'SUCCESS',
+    warning: t.type === 'WARNING',
+    danger: t.type === 'ERROR',
   }
 }
 
