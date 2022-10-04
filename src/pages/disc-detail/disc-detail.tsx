8f30f0964c2a6cd69c605072301fac4fcc8dc094
fuhaiwei <fuhaiwei@126.com>
2022-10-04 12:14:31 +0800

    feat: Update with disc-detail

--- src/pages/disc-detail/disc-detail.tsx
+++ src/pages/disc-detail/disc-detail.tsx
@@ -1,260 +1,59 @@
 import { useAppSelector } from '#A/hooks'
 import { MzHeader } from '#C/header/MzHeader'
-import { MzLink } from '#C/link/MzLink'
-import { useAjax } from '#H/useAjax'
-import { useForm } from '#H/useFrom'
+import { useLocal } from '#H/useLocal'
 import { useOnceRequest } from '#H/useOnce'
 import { fetchResult } from '#U/fetch/fetchResult'
-import { Button, Input, Modal, Radio } from 'antd'
-import { Link } from 'react-router-dom'
+import { Tabs } from 'antd'
 
-import { apiToDiscs, linkToAmazon, linkToDiscs } from '#A/links'
 import { IDisc } from '#T/disc'
-import { discTitle } from '#T/disc-utils'
-import { formatNumber } from '#U/format'
-import dayjs from 'dayjs'
+import { discTitle } from '#T/disc-comps'
 
-interface FormEdit {
-  rank?: string
-  titlePc?: string
-  discType?: string
-  releaseDate?: string
-}
+import { DiscEdit } from './disc-edit'
+import { DiscRank } from './disc-rank'
+import { DiscView } from './disc-view'
 
 interface Props {
   url: string
 }
 
 export function DiscDetail({ url }: Props) {
-  const { form, setForm, onValueChange } = useForm<FormEdit>({})
-  const [isPut, doPut] = useAjax<IDisc>('put')
-  const [isPatch, doPatch] = useAjax<IDisc>('patch')
-
-  const hasBasic = useAppSelector((state) => state.session.hasBasic)
-  const { data: disc, ...state } = useOnceRequest(
-    () => fetchResult<IDisc>(url).then((result) => result.data),
-    {
-      onSuccess: (disc) => {
-        const { titlePc, discType, releaseDate } = disc!
-        setForm({ titlePc, discType, releaseDate })
-      },
-    }
+  const { data: disc, ...state } = useOnceRequest(() =>
+    fetchResult<IDisc>(url).then((result) => result.data)
   )
 
-  function doEditDisc() {
-    if (!form.releaseDate) {
-      Modal.warning({ title: '请检查输入项', content: '请输入发售日期' })
-      return
-    }
-    if (!form.releaseDate.match(/^\d{4}\/\d{1,2}\/\d{1,2}$/g)) {
-      Modal.warning({ title: '请检查输入项', content: '格式应该为: YYYY/M/D' })
-      return
-    }
-    if (disc != null) {
-      doPut(apiToDiscs(`/${disc.id}`), '编辑碟片', {
-        body: form,
-        onSuccess: state.mutate,
-      })
-    }
-  }
-
-  function doEditRank() {
-    if (!form.rank) {
-      Modal.warning({ title: '请检查输入项', content: '请输入碟片排名' })
-      return
-    }
-    if (!form.rank.match(/^[0-9]+$/) || parseInt(form.rank) <= 0) {
-      Modal.warning({ title: '请检查输入项', content: '碟片排名必须是正整数' })
-      return
-    }
-    if (disc != null) {
-      doPatch(apiToDiscs(`/${disc.id}`), '更新排名', {
-        body: { rank: form.rank },
-        onSuccess: state.mutate,
-      })
-    }
-  }
-
-  function addAmazonLimit() {
-    setForm((draft) => {
-      draft.titlePc = draft.titlePc + '【尼限】'
-    })
-  }
-
-  const title = disc ? discTitle(disc) : undefined
+  const hasBasic = useAppSelector((state) => state.session.hasBasic)
+  const [tabKey, setTabKey] = useLocal<string>('discdetail-tabkey', 'view-disc')
 
   return (
     <div className="disc-detail" style={{ maxWidth: 650 }}>
-      <MzHeader title={{ prefix: '碟片信息', suffix: title }} state={state} />
-      {disc && (
-        <>
-          <div className="input-wrapper">
-            <div className="input-label">
-              <span>日文标题</span>
-              <span style={{ marginLeft: 20 }}>{toAmazon(disc.asin)}</span>
-            </div>
-            <Input.TextArea readOnly={true} autoSize={true} value={disc.title} />
-          </div>
-          {hasBasic && (
-            <div className="input-wrapper">
-              <div className="input-label">
-                <span>手动更新</span>
-                <span style={{ marginLeft: 20 }}>
-                  <Button type="primary" loading={isPatch} onClick={() => doEditRank()}>
-                    提交排名
-                  </Button>
-                </span>
-              </div>
-              <Input type="number" onChange={onValueChange('rank')} />
-            </div>
-          )}
-          <div className="input-wrapper">
-            <div className="input-label">
-              <span>中文标题</span>
-              <span style={{ marginLeft: 20 }}>{toRecords(disc.id)}</span>
-              <Button type="link" onClick={addAmazonLimit}>
-                +尼限
-              </Button>
-            </div>
-            <Input.TextArea
-              autoSize={true}
-              value={form.titlePc}
-              onChange={onValueChange('titlePc')}
-            />
-          </div>
-          <Input.Group compact={true}>
-            <div className="input-wrapper">
-              <Input readOnly={true} addonBefore="Id" style={{ width: 100 }} value={disc.id} />
-              <Input
-                readOnly={true}
-                addonBefore="Asin"
-                style={{ width: 180, marginLeft: 12 }}
-                value={disc.asin}
-              />
-            </div>
-          </Input.Group>
-          <Input.Group compact={true}>
-            <div className="input-wrapper">
-              <Input
-                readOnly={true}
-                addonBefore="当前"
-                style={{ width: 140 }}
-                value={formatRank(disc.thisRank)}
-              />
-              <Input
-                readOnly={true}
-                addonBefore="前回"
-                style={{ width: 140, marginLeft: 12 }}
-                value={formatRank(disc.prevRank)}
-              />
-            </div>
-          </Input.Group>
-          <Input.Group compact={true}>
-            <div className="input-wrapper">
-              <Input
-                readOnly={true}
-                addonBefore="累积PT"
-                style={{ width: 140 }}
-                value={disc.totalPt}
-              />
-              <Input
-                readOnly={true}
-                addonBefore="预测PT"
-                style={{ width: 140, marginLeft: 12 }}
-                value={disc.guessPt}
-              />
-            </div>
-          </Input.Group>
-          <Input.Group compact={true}>
-            <div className="input-wrapper">
-              <Input
-                readOnly={true}
-                addonBefore="日增PT"
-                style={{ width: 140 }}
-                value={disc.todayPt}
-              />
-              <Input
-                readOnly={true}
-                addonBefore="Nico预约"
-                style={{ width: 140, marginLeft: 12 }}
-                value={disc.nicoBook}
-              />
-            </div>
-          </Input.Group>
-          <Input.Group compact={true}>
-            <div className="input-wrapper">
-              <Input
-                addonBefore="发售"
-                style={{ width: 160 }}
-                value={form.releaseDate}
-                onChange={onValueChange('releaseDate')}
-              />
-              <Input
-                readOnly={true}
-                addonBefore="天数"
-                style={{ width: 120, marginLeft: 12 }}
-                value={disc.surplusDays}
-              />
-            </div>
-          </Input.Group>
-          <div className="input-wrapper">
-            <Input
-              readOnly={true}
-              addonBefore="创建时间"
-              style={{ width: 270 }}
-              value={formatDate(disc.createTime)}
-            />
-          </div>
-          <div className="input-wrapper">
-            <Input
-              readOnly={true}
-              addonBefore="刷新时间"
-              style={{ width: 270 }}
-              value={formatDate(disc.updateTime)}
-            />
-          </div>
-          <div className="input-wrapper">
-            <Input
-              readOnly={true}
-              addonBefore="修改时间"
-              style={{ width: 270 }}
-              value={formatDate(disc.modifyTime)}
-            />
-          </div>
-          <div className="input-wrapper">
-            <Radio.Group value={form.discType} onChange={onValueChange('discType')}>
-              <Radio.Button value="Cd">CD</Radio.Button>
-              <Radio.Button value="Bluray">BD</Radio.Button>
-              <Radio.Button value="Dvd">DVD</Radio.Button>
-              <Radio.Button value="Auto">自动</Radio.Button>
-              <Radio.Button value="Other">未知</Radio.Button>
-            </Radio.Group>
-            {hasBasic && (
-              <div style={{ marginTop: 20 }}>
-                <Button loading={isPut} type="primary" onClick={doEditDisc}>
-                  提交修改
-                </Button>
-              </div>
-            )}
-          </div>
-        </>
-      )}
+      <MzHeader title={disc && discTitle(disc)} state={state} />
+      {disc &&
+        (hasBasic ? (
+          <Tabs
+            type="card"
+            activeKey={tabKey}
+            onChange={setTabKey}
+            items={[
+              {
+                label: '查看碟片',
+                key: 'view-disc',
+                children: <DiscView disc={disc} />,
+              },
+              {
+                label: '编辑碟片',
+                key: 'edit-disc',
+                children: <DiscEdit disc={disc} setDisc={state.mutate} />,
+              },
+              {
+                label: '更新排名',
+                key: 'edit-edit-rank',
+                children: <DiscRank disc={disc} setDisc={state.mutate} />,
+              },
+            ]}
+          />
+        ) : (
+          <DiscView disc={disc} />
+        ))}
     </div>
   )
-}
-
-function formatDate(time?: number) {
-  return time == null ? '无' : dayjs(time).format('YYYY-MM-DD HH:mm:ss')
-}
-
-function formatRank(rank?: number) {
-  return `${rank ? formatNumber(rank, '****') : '----'}位`
-}
-
-function toAmazon(asin: string) {
-  return <MzLink href={linkToAmazon(asin)} title="点击打开日亚页面" />
-}
-
-function toRecords(id: number) {
-  return <Link to={linkToDiscs(`/${id}/records`)}>点击查看所有排名</Link>
 }
---
+++ src/pages/disc-detail/disc-edit.tsx
@@ -0,0 +1,85 @@
+import { useAjax } from '#H/useAjax'
+import { Rules } from '#T/antd'
+import { Button, Form, Input, Radio } from 'antd'
+
+import { apiToDiscs } from '#A/links'
+import { IDisc } from '#T/disc'
+
+interface FormEdit {
+  titlePc: string
+  discType: string
+  releaseDate: string
+}
+
+const rules: Rules = {
+  releaseDate: [
+    {
+      required: true,
+      message: '必须输入发售日期',
+    },
+    {
+      pattern: /^\d{4}\/\d{1,2}\/\d{1,2}$/,
+      message: '格式必须为[YYYY/M/D]',
+    },
+  ],
+  discType: [
+    {
+      required: true,
+      message: '必须选择碟片类型',
+    },
+  ],
+}
+
+interface Props {
+  disc: IDisc
+  setDisc: (disc: IDisc) => void
+}
+
+export function DiscEdit(props: Props) {
+  const { disc, setDisc } = props
+
+  const [isEdit, doEdit] = useAjax<IDisc>('put')
+
+  function doEditDisc(form: FormEdit) {
+    doEdit(apiToDiscs(`/${disc.id}`), '编辑碟片', {
+      body: form,
+      onSuccess: setDisc,
+    })
+  }
+
+  return (
+    <div className="disc-edit">
+      <Form
+        style={{ marginTop: 24 }}
+        labelCol={{ span: 5 }}
+        wrapperCol={{ span: 16 }}
+        initialValues={disc}
+        onFinish={doEditDisc}
+      >
+        <Form.Item label="日文标题">
+          <Input.TextArea className="readonly" readOnly={true} autoSize={true} value={disc.title} />
+        </Form.Item>
+        <Form.Item label="中文标题" name="titlePc">
+          <Input.TextArea autoSize={true} />
+        </Form.Item>
+        <Form.Item label="发售日期" name="releaseDate" rules={rules.releaseDate}>
+          <Input />
+        </Form.Item>
+        <Form.Item label="碟片类型" name="discType" rules={rules.discType}>
+          <Radio.Group>
+            <Radio.Button value="Cd">CD</Radio.Button>
+            <Radio.Button value="Bluray">BD</Radio.Button>
+            <Radio.Button value="Dvd">DVD</Radio.Button>
+            <Radio.Button value="Auto">自动</Radio.Button>
+            <Radio.Button value="Other">未知</Radio.Button>
+          </Radio.Group>
+        </Form.Item>
+        <Form.Item wrapperCol={{ offset: 5 }}>
+          <Button type="primary" htmlType="submit" loading={isEdit}>
+            提交修改
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
+  )
+}
