7d4d627d43cb989288b187a32eced6b5cbbfec0f
fuhaiwei <fuhaiwei@126.com>
2023-08-30 14:07:56 +0800

    feat: Add close website warning

--- src/commons/app/layout/App.tsx
+++ src/commons/app/layout/App.tsx
@@ -1,4 +1,4 @@
-import { Layout } from 'antd'
+import { Layout, Modal } from 'antd'
 import './App.scss'
 
 import { AppContent } from './app-content'
@@ -6,8 +6,14 @@ import { AppFooter } from './app-footer'
 import { AppHeader } from './app-header'
 import { AppLogin } from './app-login'
 import { AppSider } from './app-sider'
+import { useSession } from '#CH/useLocal'
 
 export default function App() {
+  const [show, setShow] = useSession('closesite-warning', true)
+  if (show) {
+    setShow(false)
+    Modal.warn({ title: '闭站通知', content: '因无法继续抓取日亚数据，网站即将关闭' })
+  }
   return (
     <div className="app-root">
       <Layout>
